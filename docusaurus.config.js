0710f000b466a60a79ada3dff622ad4c9a637962
NIGHTOWLgrangde <2535474943@qq.com>
2025-05-23 21:01:59 +0800

    0

---
+++ docusaurus.config.js
@@ -0,0 +1,209 @@
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
+
+import { themes as prismThemes } from 'prism-react-renderer';
+const fs = require('fs-extra');
+const path = require('path');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: '狐莱姆在盯着你...',
+  tagline: '可安可可可爱了!',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: '',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'NIGHTOWLgrangde', // Usually your GitHub org/user name.
+  projectName: 'NIGHTOWLgrangde.github.io', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
+  trailingSlash: false,
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'zh-Hans',
+    locales: ['zh-Hans'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: './sidebars.js',
+        },
+        blog: {
+          blogSidebarTitle: '乾狐离光的日记本',
+          blogSidebarCount: 10,
+          showReadingTime: true,
+
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: 'img/fox.png',
+      announcementBar: {
+        id: 'look_at_me_2',
+        content:
+          '欢迎来到我的个人主页！',
+        backgroundColor: '#FFF',
+        textColor: '#DA0',
+        isCloseable: true,
+      },
+      navbar: {
+        hideOnScroll: true,
+        title: '乾狐之家,狐狸的衣柜!',
+        logo: {
+          alt: 'My Site Logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          { to: '/blog', label: '日记本', position: 'left' },
+          {
+            type: 'dropdown',
+            label: '乾狐一家',
+            position: 'left',
+            items: [
+              {
+                label: '关于狐狸',
+                to: '/docs/intro',
+              },
+              {
+                label: '我的项目',
+                to: '/docs/game',
+              },
+            ],
+          },
+          {
+            to: '/docs/forever',
+            label: '永恒之息',
+            position: 'left',
+          },
+          {
+            to: '/docs/jrrp',
+            label: '今日人品',
+            position: 'left',
+          },
+          {
+            to: '/docs/song',
+            label: '来听歌吧',
+            position: 'left',
+          },
+          {
+            href: 'https://api.lolimi.cn/60/',
+            label: '每日60秒',
+            position: 'left',
+          },
+          {
+            href: 'https://github.com/QHMY114514',
+            label: 'MyGitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'light',
+        links: [
+          {
+            title: '联系狐狸',
+            items: [
+              {
+                label: '我的哔哩哔哩主页',
+                href: 'https://space.bilibili.com/454816001',
+              },
+              {
+                label: '我的个人群(QQ)',
+                href: 'https://qm.qq.com/q/bImTc3BPsA',
+              },
+              {
+                label: 'Flime官方群(QQ)',
+                href: 'https://qm.qq.com/q/qatSwAhTQk',
+              }
+            ],
+          },
+          {
+            title: '你想找什么?',
+            items: [
+              {
+                label: '狐狸是谁?',
+                to: '/docs/intro',
+              },
+              {
+                label: '狐狸怎么叫?',
+                to: '/blog',
+              },
+              {
+                label: '小玩具',
+                to: '/docs/game',
+              },
+              {
+                label: '狐狸的GitHub主页',
+                href: 'https://github.com/QHMY114514',
+              },
+              {
+                label: 'FlimeBot',
+                href: 'https://flime.online',
+              },
+            ],
+          },
+          {
+            title: '友情链接',
+            items: [
+              {
+                label: '户山兔兔的小站',
+                href: 'https://danieltoyama.fun',
+              },
+              {
+                label: 'YukieBot(by 户山兔兔)',
+                href: 'https://yukiebot.top',
+              },
+              {
+                label: 'KasumiBot(by 户山兔兔)',
+                href: 'https://ksmbot.top',
+              },
+              {
+                label: '桑帛云API',
+                href: 'https://api.lolimi.cn',
+              },
+              {
+                label: '孤影墨香',
+                href: 'https://www.iloli.xin/',
+              }
+            ],
+          },
+        ],
+        copyright: ``,
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      }
+    }),
+  // Add the Cloudflare Web Analytics script
+  scripts: [
+  ],
+};
+
+export default config;
---
+++ src/pages/index.js
@@ -0,0 +1,177 @@
+import React, { useState, useEffect } from 'react';
+import Link from '@docusaurus/Link';
+import Layout from '@theme/Layout';
+
+import './index.css'
+
+export default function () {
+
+    // 定义背景图片链接数组
+    const desktopBackgroundImages = [
+        "https://api.oick.cn/random/api.php?type=pc",
+        "https://t.mwm.moe/fj",
+        "https://t.mwm.moe/pc",
+        "https://api.miaomc.cn/image/get",
+        "https://bing.img.run/rand_1366x768.php",
+        "https://api.mtyqx.cn/api/random.php"
+    ];
+
+    const mobileBackgroundImages = [
+        "https://api.oick.cn/random/api.php?type=pe",
+        "https://api.suyanw.cn/api/mao/",
+        "https://t.mwm.moe/mp",
+        "https://moe.jitsu.top/api",
+    ];
+
+    // 判断屏幕比例是否接近手机（高宽比大于1）
+    var isMobileRatioOld;
+    const isMobileRatio = () => {
+        const aspectRatio = window.innerHeight / window.innerWidth;
+        return aspectRatio > 1;
+    };
+
+    // 定义状态存储背景图片
+    const [backgroundImage, setBackgroundImage] = useState('');
+
+    // 随机选择背景图片
+    const selectRandomBackgroundImage = () => {
+        const images = isMobileRatio() ? mobileBackgroundImages : desktopBackgroundImages;
+        const randomIndex = Math.floor(Math.random() * images.length);
+        setBackgroundImage(images[randomIndex]);
+    };
+
+    const [logoColor, setLogoColor] = useState('');
+    const [openTime, setOpenTime] = useState('');
+    const [stayTime, setStayTime] = useState('');
+
+    // 在组件挂载时选择背景图片
+    useEffect(() => {
+        var cometime = new Date();
+
+        update();
+        setInterval(() => {
+            update();
+        }, 100);
+
+        function update() {
+            setLogoColor(`hsl(${(new Date() / 30) % 360},50%,70%)`);
+            setOpenTime(TimetoTextDay(new Date() - 1732948276347));
+            setStayTime(TimetoTextStay(new Date() - cometime));
+        }
+
+        function TimetoTextDay(time) {
+            return `${Math.round(time / 864) / 100000}天`;
+        }
+        
+        function TimetoTextStay(time) {
+            if (time <= 10000) {
+                return `${Math.round(time / 1000)}秒,欢迎你的来访٩(๑❛ᴗ❛๑)۶`;
+            }
+            else if (time < 30000) {
+                return `${Math.round(time / 1000)}秒,这里是文案!你好呀~(oﾟ▽ﾟ)o  `;
+            }
+            else if (time < 60000) {
+                return `${Math.round(time / 1000)}秒,还在找想看的东西吗?ヾ(=･ω･=)o`;
+            }
+            else if (time < 90000) {
+                return `${Math.round(time / 600) / 100}分,原来你是喜欢我吗,在这里呆这么久(*/ω＼*)`;
+            }
+            else if (time < 120000) {
+                return `${Math.round(time / 600) / 100}分,这条文案不喜欢发颜文字!!!`;
+            }
+            else if (time < 180000) {
+                return `${Math.round(time / 600) / 100}分,你在和我一起发呆吗(｀・ω・´)`;
+            }
+            else if (time < 240000) {
+                return `${Math.round(time / 600) / 100}分,哇塞!你在这里三分钟了!( • ̀ω•́ )✧`;
+            }
+            else if (time < 300000) {
+                return `${Math.round(time / 600) / 100}分,你想知道下一次这行字什么时候变化吗?`;
+            }
+            else if (time < 360000) {
+                return `${Math.round(time / 600) / 100}分,答案是五分钟!!!o(￣ε￣*) `;
+            }
+            else if (time < 480000) {
+                return `${Math.round(time / 600) / 100}分,嘻嘻~~~杂鱼~杂鱼~❤~见到我就走不开了~`;
+            }
+            else if (time < 600000) {
+                return `${Math.round(time / 600) / 100}分,后面没有了,相信我☆(ゝω･)v`;
+            }
+            else if (time < 900000) {
+                return `${Math.round(time / 600) / 100}分,好像有笨蛋忘记关浏览器了~~~o(´^｀)o哼哼哼!`;
+            }
+            else if (time < 1200000) {
+                return `${Math.round(time / 600) / 100}分,你住在这了???┌(。Д。)┐`;
+            }
+            else if (time < 1800000) {
+                return `${Math.round(time / 600) / 100}分,Σ(っ°Д°;)っ 404 找不到文案`;
+            }
+            else if (time < 2700000) {
+                return `${Math.round(time / 600) / 100}分,ʅ( ´・∧・｀)ʃ 我投降了!`;
+            }
+            else if (time < 3600000) {
+                return `${Math.round(time / 600) / 100}分,为你颁发─=≡Σ((( つ•̀ω•́)つ发呆达人`;
+            }
+            else {
+                return `${Math.round(time / 360) / 10000}小时,♪＼( >w<)︿(QwQ )／♪`;
+            }
+        }
+        
+        // 初始化时选择背景图片
+        selectRandomBackgroundImage();
+        isMobileRatioOld = isMobileRatio();
+
+        // 监听窗口大小变化，动态调整背景图片
+        const handleResize = () => {
+            if (isMobileRatio() == isMobileRatioOld) return;
+            isMobileRatioOld = isMobileRatio();
+            selectRandomBackgroundImage();
+        };
+
+        // 添加resize事件监听器
+        window.addEventListener('resize', handleResize);
+
+        // 清理事件监听器
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+
+    return (
+        <Layout description='乾狐のBlog站，乾狐的小站，乾狐离光，乾狐梦影，QHMY, QHLG, Flime'>
+            <div id='index-page-container'>
+                {/* Set the background image dynamically */}
+                <div id='index-page-bg' style={{ backgroundImage: `url(${backgroundImage})` }}></div>
+                <div id='index-page-bg-mask'></div>
+                <div id='index-page-main'>
+                    <div id='logo' style={{ textAlign: "center", color: `${logoColor}` }}>
+                        <span>一只快速的Flime跳过了懒惰的QHLG</span>
+                    </div>
+                    <div id='index-page-description' style={{ textAlign: "center"}}>
+                        欢迎来到我的小站!想来点什么?<br />本站已启用{openTime}<br />你已经在本页面{stayTime}
+                    </div>
+                    <div id='index-page-button-container'>
+                        <div id='index-page-button'>
+                            <Link
+                                className="button button--secondary button--lg"
+                                to="./blog">
+                                偷看日记
+                            </Link>
+                            <Link
+                                className="button button--secondary button--lg"
+                                to="./docs/intro">
+                                抱走狐狸
+                            </Link>
+                            <Link
+                                className="button button--secondary button--lg"
+                                to="./docs/game">
+                                看看游戏
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </Layout>
+    );
+}
